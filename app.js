12bf07cf7ef28ccd715ae2ca7beb13cf1834d515
coszach@hotmail.it <coszach@hotmail.it>
2022-02-16 15:39:55 +0100

    nuova repositorie

---
+++ app.js
@@ -0,0 +1,70 @@
+// l'utente deve selezionare la dificoltà del gioco
+  // collegare il bottone alla scelta dell'utente
+const play = document.getElementById('play');
+const easy = document.getElementById('easy').value;
+const hard = document.getElementById('hard').value;
+const crazy = document.getElementById('crazy').value;
+const grid = document.querySelector('.grid');
+let squareElement;
+let numero = 0;
+let numeroRighe = 0;
+let numeroColonne = 0;
+
+
+// FUNZIONI
+
+function difficultyGrid(){
+  const dificoltà = document.getElementById('choose-difficulty').value;
+  switch (dificoltà) {
+    case easy :
+      numeroRighe = 10;
+      numeroColonne = 10;
+      return numeroColonne;
+    case hard :
+      numeroRighe = 9;
+      numeroColonne = 9;
+      return numeroColonne;
+    case crazy : 
+    numeroRighe = 7;
+      numeroColonne = 7;
+      return numeroColonne;
+    }
+}
+
+function cellaCallBack(){
+  this.classList.add('square-select');
+  let element = this.innerHTML
+  console.log(element)
+  this.removeEventListener('click', cellaCallBack)
+}
+
+function createSquareElement (){
+  for (let i = 0; i < Math.pow(numeroColonne, 2); i++){
+    squareElement = document.createElement('div');
+    squareElement.classList.add('square');
+    squareElement.style.width = `calc(100% / ${numeroRighe})`;
+    grid.append(squareElement);
+    numero = i + 1;
+    squareElement.append(numero);
+    squareElement.addEventListener('click', cellaCallBack);
+  }
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function startGame(){
+  difficultyGrid();
+  grid.innerHTML='';
+  createSquareElement();
+
+}
+  
+
+
+// applico la funziona al click del bottone play
+play.addEventListener('click', startGame);
